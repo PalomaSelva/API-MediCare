b5877b43a22567dded12c503fb35a1bbb2da023e
PalomaSelva <palomaselva1@email.com>
2024-11-03 21:18:25 -0300

    feat: Controller authenticate

---
+++ src/http/controllers/authenticate.ts
@@ -0,0 +1,45 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { AuthenticateUseCase } from "@/use-cases/authenticate";
+import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials";
+
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, { message: "A senha deve conter, no mínimo, 8 caracteres." })
+      .refine((password) => /[A-Z]/.test(password), {
+        message: "A senha deve conter letras maiúsculas.",
+      })
+      .refine((password) => /[a-z]/.test(password), {
+        message: "A senha deve conter letras minúsculas.",
+      })
+      .refine((password) => /[0-9]/.test(password), {
+        message: "A senha deve conter números.",
+      })
+      .refine((password) => /[!@#$%^&*]/.test(password), {
+        message: "A senha deve conter caracteres especiais.",
+      }),
+  });
+
+  const { email, password } = authenticateBodySchema.parse(request.body);
+
+  try {
+    const prismaUsersRepository = new PrismaUsersRepository();
+    const authenticateUseCase = new AuthenticateUseCase(prismaUsersRepository);
+
+    await authenticateUseCase.execute({ email, password });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(409).send({ message: error.message });
+    }
+    throw error;
+  }
+  return reply.status(200).send();
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
 import { app } from "@/app";
 import { registerPatient } from "./controllers/register-patient";
+import { authenticate } from "./controllers/authenticate";
 
 export async function appRoutes() {
-  app.post("/user", registerPatient);
+  app.post("/users", registerPatient);
+  app.post("/sessions", authenticate);
 }
