2750ad4c699cf9c440ec7a4481e10dda5c066d6e
PalomaSelva <palomaselva1@email.com>
2024-11-06 15:21:29 -0300

    remove: importações

--- src/http/controllers/register-doctor.ts
+++ src/http/controllers/register-doctor.ts
@@ -1,6 +1,5 @@
 import { UserAlreadyExistsError } from "@/use-cases/errors/user-already-exists";
 import { makeRegisterDoctorUseCase } from "@/use-cases/factories/make-register-doctor-use-case";
-import { makeRegisterPatientUseCase } from "@/use-cases/factories/make-register-patient-use-case";
 
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
