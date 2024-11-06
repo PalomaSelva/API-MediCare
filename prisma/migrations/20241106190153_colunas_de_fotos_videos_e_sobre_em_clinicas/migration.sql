37dfab7da5aa77e463b3a5a131bb6d5a5343c0c1
PalomaSelva <palomaselva1@email.com>
2024-11-06 16:03:47 -0300

    fest: Colunas de fotos, videos em Clinic e tempo médio de atendimento em Doctor

---
+++ prisma/migrations/20241106190153_colunas_de_fotos_videos_e_sobre_em_clinicas/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Clinic" ADD COLUMN     "about_me" TEXT,
+ADD COLUMN     "photo_01" TEXT,
+ADD COLUMN     "photo_02" TEXT,
+ADD COLUMN     "photo_03" TEXT,
+ADD COLUMN     "video" TEXT;
---
+++ prisma/migrations/20241106190251_coluna_de_tempo_medio_de_atendimento/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Doctor" ADD COLUMN     "averageAppointmentDuration" INTEGER NOT NULL DEFAULT 30;
